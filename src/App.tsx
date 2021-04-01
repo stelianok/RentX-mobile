e6dea0ad31da893f4cb7b863e036522bec88df46
Kauã Steliano <stelianok@gmail.com>
2021-04-01 13:18:18 -0300

    Configured project.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text style={{fontFamily: 'ArchivoNarrow-Medium', color: '#fff'}}>
+        RentX
+      </Text>
+    </View>
+  );
+};
+export default App;
