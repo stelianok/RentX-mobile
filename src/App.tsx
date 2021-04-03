18cbd1dd0273f6dbc7e8b58e8011c76806e48f7d
Kauã Steliano <stelianok@gmail.com>
2021-04-03 16:08:23 -0300

    Removed StatusBar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
 import SignIn from './screens/SignIn';
 
 const App: React.FC = () => {
   return (
     <>
-      <StatusBar translucent={true} backgroundColor={'transparent'} />
       <SignIn />
     </>
   );
