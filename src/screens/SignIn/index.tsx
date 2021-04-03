7727f6ed76a74d6d76c66bd4e5f2ebff0180220e
Kauã Steliano <stelianok@gmail.com>
2021-04-03 14:23:29 -0300

    Finished SignIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,50 +1,95 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 import {
   Container,
   Header,
   GoBackButton,
   Content,
+  TextArea,
   Title,
   Subtitle,
   Form,
+  InputContainer,
+  IconContainer,
+  Input,
+  ChangePasswordVisibilityButton,
   Buttons,
-  AlignButtonsInRow,
   ForgotPasswordButton,
   ForgotPasswordButtonText,
   LoginButton,
   LoginButtonText,
 } from './styles';
 
+import Password from '../../assets/icons/password.svg';
+import Email from '../../assets/icons/email.svg';
+import {StatusBar} from 'react-native';
+
 const SignIn: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <Container>
-      <Header>
-        <GoBackButton>
-          <Icon name={'chevron-left'} size={18} color={'#AEAEB3'} />
-        </GoBackButton>
-      </Header>
-      <Content>
-        <Title>Estamos quase lá</Title>
-        <Subtitle>
-          Faça seu login para começar uma experiênciaincrível.
-        </Subtitle>
-        <Form>
-          <Buttons>
-            <AlignButtonsInRow>
+    <>
+      <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'} />
+      <Container>
+        <Header>
+          <GoBackButton>
+            <Icon name={'chevron-left'} size={24} color={'#AEAEB3'} />
+          </GoBackButton>
+        </Header>
+        <Content>
+          <TextArea>
+            <Title>Estamos quase lá.</Title>
+            <Subtitle>
+              Faça seu login para começar uma experiência incrível.
+            </Subtitle>
+          </TextArea>
+          <Form>
+            <InputContainer>
+              <IconContainer>
+                <Email width={28} height={28} />
+              </IconContainer>
+              <Input
+                placeholder={'E-mail'}
+                placeholderTextColor={'#AEAEB3'}
+                keyboardType={'email-address'}
+                onChangeText={text => setEmail(text)}
+                value={email}
+              />
+            </InputContainer>
+            <InputContainer>
+              <IconContainer>
+                <Password width={28} height={28} />
+              </IconContainer>
+              <Input
+                placeholder={'Senha'}
+                placeholderTextColor={'#AEAEB3'}
+                secureTextEntry={true}
+                onChangeText={text => setPassword(text)}
+                value={password}
+              />
+              <ChangePasswordVisibilityButton
+                onPress={() => {
+                  console.log('lol');
+                }}>
+                <IconContainer>
+                  <Icon name={'eye'} size={28} color={'#47474d'} />
+                </IconContainer>
+              </ChangePasswordVisibilityButton>
+            </InputContainer>
+            <Buttons>
               <ForgotPasswordButton>
                 <ForgotPasswordButtonText>
                   Esqueci minha senha.
                 </ForgotPasswordButtonText>
               </ForgotPasswordButton>
-            </AlignButtonsInRow>
-            <LoginButton>
-              <LoginButtonText>Login</LoginButtonText>
-            </LoginButton>
-          </Buttons>
-        </Form>
-      </Content>
-    </Container>
+              <LoginButton>
+                <LoginButtonText>Login</LoginButtonText>
+              </LoginButton>
+            </Buttons>
+          </Form>
+        </Content>
+      </Container>
+    </>
   );
 };
 
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,16 +1,99 @@
-import {RectButton} from 'react-native-gesture-handler';
+import {BorderlessButton, RectButton} from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
-export const Header = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  padding: 20px;
+`;
+export const Header = styled.View`
+  margin-bottom: 120px;
+`;
 export const GoBackButton = styled.TouchableOpacity``;
-export const Content = styled.View``;
-export const Title = styled.Text``;
-export const Subtitle = styled.Text``;
-export const Form = styled.View``;
-export const Buttons = styled.View``;
-export const AlignButtonsInRow = styled.View``;
-export const ForgotPasswordButton = styled.TouchableOpacity``;
-export const ForgotPasswordButtonText = styled.Text``;
-export const LoginButton = styled(RectButton)``;
-export const LoginButtonText = styled.Text``;
+export const Content = styled.KeyboardAvoidingView`
+  justify-content: flex-start;
+`;
+
+export const TextArea = styled.View`
+  margin-right: 150px;
+`;
+export const Title = styled.Text`
+  color: #47474d;
+  text-align: left;
+
+  font-family: 'ArchivoNarrow-SemiBold';
+  font-weight: bold;
+  font-size: 40px;
+
+  margin-bottom: 15px;
+`;
+export const Subtitle = styled.Text`
+  color: #7a7a80;
+  text-align: left;
+
+  font-family: 'Inter-Regular';
+  font-size: 15px;
+`;
+export const Form = styled.View`
+  margin-top: 100px;
+`;
+
+export const InputContainer = styled.View`
+  flex-direction: row;
+
+  justify-content: flex-start;
+  align-items: center;
+  align-content: center;
+
+  margin-bottom: 5px;
+`;
+export const IconContainer = styled.View`
+  background-color: #f5f5fa;
+
+  padding: 15px;
+`;
+export const Input = styled.TextInput`
+  flex: 1;
+  background-color: #f5f5fa;
+  color: #000;
+  font-family: 'Inter-Regular';
+
+  padding: 15px;
+  margin-left: 5px;
+`;
+
+export const ChangePasswordVisibilityButton = styled.TouchableOpacity``;
+export const Buttons = styled.View`
+  flex: 1;
+`;
+export const ForgotPasswordButton = styled.TouchableOpacity`
+  align-self: flex-end;
+  align-content: center;
+
+  margin-top: 20px;
+`;
+export const ForgotPasswordButtonText = styled.Text`
+  color: #737380;
+
+  font-family: 'Inter-Regular';
+`;
+export const LoginButton = styled(RectButton)`
+  background-color: #dc1637;
+  width: 100%;
+
+  justify-content: center;
+  align-items: center;
+
+  padding: 18px;
+
+  margin-top: 80px;
+`;
+export const LoginButtonText = styled.Text`
+  color: #fff;
+  font-family: 'Inter-Regular';
+  font-size: 18px;
+`;
