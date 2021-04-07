958f50a77df324ce4946112bd81f909057ffd2dd
Kauã Steliano <stelianok@gmail.com>
2021-04-07 09:09:02 -0300

    Created passwordInput component.

---
+++ src/components/PasswordInput/index.tsx
@@ -0,0 +1,52 @@
+import React, {useCallback, useState} from 'react';
+import {TextInputProps} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  InputContainer,
+  IconContainer,
+  InputText,
+  ChangePasswordVisibilityButton,
+} from './styles';
+
+interface InputProps extends TextInputProps {
+  IconComponent: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+}
+const PasswordInput: React.FC<InputProps> = ({IconComponent, ...rest}) => {
+  const [isPasswordProtected, setIsPasswordProtected] = useState(true);
+  const [eyeIconName, setEyeIconName] = useState('eye');
+
+  const handleEyeIcon = useCallback(() => {
+    isPasswordProtected ? setEyeIconName('eye') : setEyeIconName('eye-off');
+  }, [isPasswordProtected]);
+
+  const handlePasswordVisibility = useCallback(() => {
+    setIsPasswordProtected(!isPasswordProtected);
+    handleEyeIcon();
+  }, [handleEyeIcon, isPasswordProtected]);
+
+  return (
+    <Container>
+      <InputContainer>
+        <IconContainer>
+          <IconComponent width={28} height={28} />
+        </IconContainer>
+        <InputText
+          placeholderTextColor={'#AEAEB3'}
+          secureTextEntry={isPasswordProtected}
+          {...rest}
+        />
+        <ChangePasswordVisibilityButton
+          onPress={() => {
+            handlePasswordVisibility();
+          }}>
+          <IconContainer>
+            <Icon name={eyeIconName} size={28} color={'#47474d'} />
+          </IconContainer>
+        </ChangePasswordVisibilityButton>
+      </InputContainer>
+    </Container>
+  );
+};
+export default PasswordInput;
