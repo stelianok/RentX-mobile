fd25b1450d11b3db13955ccf928cc79ebb9c48b1
Kauã Steliano <stelianok@gmail.com>
2021-04-07 10:27:07 -0300

    Fixed status bar  inconstancy

--- src/screens/OnboardingScreen/index.tsx
+++ src/screens/OnboardingScreen/index.tsx
@@ -4,7 +4,7 @@ import Icon from 'react-native-vector-icons/Feather';
 
 import Onboarding from 'react-native-onboarding-swiper';
 import OnboardingItem from '../../components/OnboardingItem';
-import {StyleSheet} from 'react-native';
+import {StatusBar, StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/core';
 
 const styles = StyleSheet.create({
@@ -32,40 +32,43 @@ const styles = StyleSheet.create({
 const OnboardingScreen: React.FC = () => {
   const navigation = useNavigation();
   return (
-    <Onboarding
-      nextLabel={<Icon name={'chevron-right'} size={24} color={'#AEAEB3'} />}
-      bottomBarHighlight={false}
-      showSkip={false}
-      containerStyles={{
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-      }}
-      transitionAnimationDuration={500}
-      titleStyles={styles.title}
-      subTitleStyles={styles.subtitle}
-      allowFontScaling={false}
-      controlStatusBar={false}
-      onDone={() => navigation.navigate('Welcome')}
-      pages={[
-        {
-          backgroundColor: '#fff',
-          image: (
-            <OnboardingItem iconName={'RedCalendar'} screenNumber={'01'} />
-          ),
-          title: 'Primeiro,\nescolha a data',
-          subtitle:
-            'Você é quem define um período, e nós mostraremos' +
-            'os carros disponíveis',
-        },
-        {
-          backgroundColor: '#fff',
-          image: <OnboardingItem iconName={'RedCar'} screenNumber={'02'} />,
-          title: 'Depois,\nescolha o carro',
-          subtitle:
-            'Vários modelos para você dirigir seguro, com conforto e segurança.',
-        },
-      ]}
-    />
+    <>
+      <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'} />
+      <Onboarding
+        nextLabel={<Icon name={'chevron-right'} size={24} color={'#AEAEB3'} />}
+        bottomBarHighlight={false}
+        showSkip={false}
+        containerStyles={{
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+        }}
+        transitionAnimationDuration={500}
+        titleStyles={styles.title}
+        subTitleStyles={styles.subtitle}
+        allowFontScaling={false}
+        controlStatusBar={false}
+        onDone={() => navigation.navigate('Welcome')}
+        pages={[
+          {
+            backgroundColor: '#fff',
+            image: (
+              <OnboardingItem iconName={'RedCalendar'} screenNumber={'01'} />
+            ),
+            title: 'Primeiro,\nescolha a data',
+            subtitle:
+              'Você é quem define um período, e nós mostraremos' +
+              'os carros disponíveis',
+          },
+          {
+            backgroundColor: '#fff',
+            image: <OnboardingItem iconName={'RedCar'} screenNumber={'02'} />,
+            title: 'Depois,\nescolha o carro',
+            subtitle:
+              'Vários modelos para você dirigir seguro, com conforto e segurança.',
+          },
+        ]}
+      />
+    </>
   );
 };
 
--- src/screens/WelcomeScreen/index.tsx
+++ src/screens/WelcomeScreen/index.tsx
@@ -1,5 +1,6 @@
 import {useNavigation} from '@react-navigation/core';
 import React from 'react';
+import {StatusBar} from 'react-native';
 
 import {
   Container,
@@ -21,35 +22,38 @@ const WelcomeScreen: React.FC = () => {
   const navigation = useNavigation();
 
   return (
-    <Container>
-      <Union height={50} width={80} />
-      <Content>
-        <Title>{'Seja \nBem-vindo'}</Title>
-        <Subtitle>O que você deseja fazer?</Subtitle>
-      </Content>
-      <Footer>
-        <Buttons>
-          <SignInButton
+    <>
+      <StatusBar backgroundColor={'#1b1b1f'} barStyle={'light-content'} />
+      <Container>
+        <Union height={50} width={80} />
+        <Content>
+          <Title>{'Seja \nBem-vindo'}</Title>
+          <Subtitle>O que você deseja fazer?</Subtitle>
+        </Content>
+        <Footer>
+          <Buttons>
+            <SignInButton
+              onPress={() => {
+                navigation.navigate('SignIn');
+              }}>
+              <SignInText>Login</SignInText>
+            </SignInButton>
+            <SignUpButton
+              onPress={() => {
+                navigation.navigate('SignUp01');
+              }}>
+              <SignUpText>Cadastro</SignUpText>
+            </SignUpButton>
+          </Buttons>
+          <BackButton
             onPress={() => {
-              navigation.navigate('SignIn');
+              navigation.goBack();
             }}>
-            <SignInText>Login</SignInText>
-          </SignInButton>
-          <SignUpButton
-            onPress={() => {
-              navigation.navigate('SignUp01');
-            }}>
-            <SignUpText>Cadastro</SignUpText>
-          </SignUpButton>
-        </Buttons>
-        <BackButton
-          onPress={() => {
-            navigation.goBack();
-          }}>
-          <BackButtonText>Voltar</BackButtonText>
-        </BackButton>
-      </Footer>
-    </Container>
+            <BackButtonText>Voltar</BackButtonText>
+          </BackButton>
+        </Footer>
+      </Container>
+    </>
   );
 };
 export default WelcomeScreen;
