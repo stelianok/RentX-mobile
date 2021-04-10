8ff962ccfe3eec20e88b7cf77be341dbf6d316a5
Kauã Steliano <stelianok@gmail.com>
2021-04-10 11:45:56 -0300

    Created structure of Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -19,6 +19,64 @@ import {
   FilterIcon,
 } from './styles';
 
+import Audi from '../../assets/images/Audi.png';
+import Corvete from '../../assets/images/Corvete.png';
+import Lambo from '../../assets/images/Lambo.png';
+import Lancer from '../../assets/images/Lancer.png';
+import Porche from '../../assets/images/Porche.png';
+import Volvo from '../../assets/images/Volvo.png';
+
+interface Car {
+  brand: string;
+  name: string;
+  image: any;
+  price: number;
+  fuel_type: 'eletric' | 'gas' | 'alcohol';
+}
+const cars: Car[] = [
+  {
+    brand: 'LAMBORGHINI',
+    name: 'Hurracan',
+    image: Lambo,
+    price: 580,
+    fuel_type: 'eletric',
+  },
+  {
+    brand: 'AUDI',
+    name: 'RS 5 Coupé',
+    image: Audi,
+    price: 470,
+    fuel_type: 'eletric',
+  },
+  {
+    brand: 'PORCHE',
+    name: 'Panameraa',
+    image: Porche,
+    price: 235,
+    fuel_type: 'eletric',
+  },
+  {
+    brand: 'CHEVROLET',
+    name: 'Corvette Z06',
+    image: Corvete,
+    price: 943,
+    fuel_type: 'eletric',
+  },
+  {
+    brand: 'VOLVO',
+    name: 'XC40',
+    image: Volvo,
+    price: 260,
+    fuel_type: 'eletric',
+  },
+  {
+    brand: 'MITSUBISHI',
+    name: 'Lancer EVO X',
+    image: Lancer,
+    price: 605,
+    fuel_type: 'eletric',
+  },
+];
 const Home: React.FC = () => {
   return (
     <>
@@ -47,8 +105,20 @@ const Home: React.FC = () => {
               </FilterButton>
             </RowContainer>
           </ResultContainer>
-          <CarCard />
-          <CarCard />
+          <CarCard
+            name={'Hurracan'}
+            brand={'LAMBORGHINI'}
+            price={580}
+            fuel_type={'eletric'}
+            image={Lambo}
+          />
+          <CarCard
+            name={'Panamera'}
+            brand={'PORCHE'}
+            price={800}
+            fuel_type={'gas'}
+            image={Porche}
+          />
         </Content>
       </Container>
     </>
