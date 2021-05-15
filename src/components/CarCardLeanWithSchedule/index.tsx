d63693996af7bf07333af7ac0f16f1ac65cb002c
Kauã Steliano <stelianok@gmail.com>
2021-05-15 09:32:44 -0300

    Finished Designing Car Card with schedule attached.

---
+++ src/components/CarCardLeanWithSchedule/index.tsx
@@ -0,0 +1,80 @@
+import React, {useCallback} from 'react';
+import {
+  CarInfo,
+  TextArea,
+  Subtitle,
+  Name,
+  Price,
+  ImageContainer,
+  IconContainer,
+  CarImg,
+  EnergyIcon,
+  LeafIcon,
+  GasIcon,
+  RowContainer,
+  Container,
+  Footer,
+  ScheduleTitle,
+  ScheduleDateContainer,
+  ScheduleDate,
+  ArrowIcon,
+} from './styles';
+
+interface IProps {
+  brand: string;
+  name: string;
+  image: any;
+  price: number;
+  fuel_type?: 'eletric' | 'gas' | 'alcohol';
+}
+
+const CarCardLeanWithSchedule: React.FC<IProps> = ({
+  brand,
+  name,
+  image,
+  price,
+  fuel_type,
+}) => {
+  const ChooseIcon = useCallback(() => {
+    if (fuel_type === 'eletric') {
+      return <EnergyIcon width={25} height={25} />;
+    }
+    if (fuel_type === 'alcohol') {
+      return <LeafIcon width={25} height={25} />;
+    }
+
+    return <GasIcon width={25} height={25} />;
+  }, [fuel_type]);
+
+  return (
+    <>
+      <Container android_ripple={{color: '#aeaeb3'}}>
+        <CarInfo>
+          <TextArea>
+            <Subtitle>{brand}</Subtitle>
+            <Name>{name}</Name>
+          </TextArea>
+          <RowContainer>
+            <TextArea>
+              <Subtitle>AO DIA</Subtitle>
+              <Price>R$ {price}</Price>
+            </TextArea>
+            <IconContainer>{ChooseIcon()}</IconContainer>
+          </RowContainer>
+        </CarInfo>
+        <ImageContainer>
+          <CarImg source={image} resizeMode={'contain'} />
+        </ImageContainer>
+      </Container>
+      <Footer>
+        <ScheduleTitle>PERÍODO</ScheduleTitle>
+        <ScheduleDateContainer>
+          <ScheduleDate>18 Junho 2021</ScheduleDate>
+          <ArrowIcon width={24} height={24} />
+          <ScheduleDate>25 Junho 2021</ScheduleDate>
+        </ScheduleDateContainer>
+      </Footer>
+    </>
+  );
+};
+export default CarCardLeanWithSchedule;
---
+++ src/components/CarCardLeanWithSchedule/styles.ts
@@ -0,0 +1,127 @@
+import {Image} from 'react-native';
+import styled from 'styled-components/native';
+
+import Energy from '../../assets/icons/energy.svg';
+import Leaf from '../../assets/icons/leaf.svg';
+import Gas from '../../assets/icons/gas.svg';
+import Arrow from '../../assets/icons/arrow_right.svg';
+
+export const Container = styled.Pressable`
+  flex: 1;
+
+  background-color: #f4f5f6;
+
+  border-radius: 5px;
+
+  padding: 15px;
+
+  flex-direction: row;
+`;
+
+export const CarInfo = styled.View`
+  justify-content: space-between;
+`;
+
+export const TextArea = styled.View``;
+
+export const Subtitle = styled.Text`
+  font-family: 'ArchivoNarrow-Medium';
+  font-size: 12px;
+
+  color: #aeaeb3;
+`;
+
+export const Name = styled.Text`
+  font-family: 'ArchivoNarrow-Medium';
+  font-size: 20px;
+
+  color: #47474d;
+`;
+
+export const Price = styled.Text`
+  font-family: 'ArchivoNarrow-Medium';
+  font-size: 20px;
+
+  color: #dc1637;
+`;
+
+export const ImageContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  margin-left: 10px;
+`;
+
+export const CarImg = styled(Image)`
+  height: 90px;
+`;
+
+export const IconContainer = styled.View`
+  flex-direction: row;
+
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+
+  margin-top: 9px;
+  margin-left: 25px;
+`;
+
+export const EnergyIcon = styled(Energy)`
+  color: #aeaeb3;
+`;
+
+export const LeafIcon = styled(Leaf)`
+  color: #aeaeb3;
+`;
+
+export const GasIcon = styled(Gas)`
+  color: #aeaeb3;
+`;
+
+export const RowContainer = styled.View`
+  flex-direction: row;
+
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 25px;
+`;
+
+export const Footer = styled.View`
+  flex: 1;
+  flex-direction: row;
+
+  justify-content: center;
+  align-items: center;
+
+  background-color: #f4f5f6;
+
+  margin-top: 3px;
+  margin-bottom: 15px;
+
+  padding: 10px;
+  padding-left: 18px;
+`;
+export const ScheduleTitle = styled.Text`
+  font-family: 'ArchivoNarrow-Medium';
+  font-size: 11px;
+  color: #aeaeb3;
+
+  align-self: center;
+  margin-right: 15px;
+`;
+export const ScheduleDateContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+export const ScheduleDate = styled.Text`
+  font-family: 'Inter-Regular';
+  font-size: 13px;
+  color: #47474d;
+`;
+export const ArrowIcon = styled(Arrow)``;
