59a9f2da7a84a26504615e26587a48d5bd03a366
Kauã Steliano <stelianok@gmail.com>
2021-05-19 09:01:18 -0300

    Created CarDetails Structure, finished designing 75% of the screen.

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+  BorderlessButton,
+  GoBackIcon,
+  Pagination,
+  Dot,
+  InactiveDot,
+  Content,
+  CarImageContainer,
+  CarImage,
+  CarBasicInfoContainer,
+  CarInfo,
+  DailyPrice,
+  Title,
+  Info,
+  Specifications,
+  Specification,
+  SpeedIcon,
+  SpecificationInfo,
+  UpIcon,
+  StrenghtIcon,
+  FuelIcon,
+  GearIcon,
+  UserIcon,
+  ScheduleContainer,
+  DateContainer,
+  DateInfo,
+  ArrowRightIcon,
+  Footer,
+  PriceContainer,
+  DayValue,
+  TotalValue,
+  SubmitButton,
+  SubmitButtonText,
+} from './styles';
+
+import Lambo from '../../assets/images/Lambo.png';
+import {StatusBar} from 'react-native';
+
+const CarDetails: React.FC = () => {
+  return (
+    <>
+      <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'} />
+      <Container>
+        <Header>
+          <BorderlessButton
+            android_ripple={{color: '#AEAEB3', borderless: true}}>
+            <GoBackIcon name={'chevron-left'} size={24} color={'#AEAEB3'} />
+          </BorderlessButton>
+          <Pagination>
+            <Dot>•</Dot>
+            <InactiveDot>•</InactiveDot>
+            <InactiveDot>•</InactiveDot>
+            <InactiveDot>•</InactiveDot>
+          </Pagination>
+        </Header>
+        <Content>
+          <CarImageContainer>
+            <CarImage source={Lambo} resizeMode={'contain'} />
+          </CarImageContainer>
+          <CarBasicInfoContainer>
+            <CarInfo>
+              <Title>LAMBORGHINI</Title>
+              <Info>Hurracan</Info>
+            </CarInfo>
+            <CarInfo>
+              <Title>AO DIA</Title>
+              <DailyPrice>R$ 580</DailyPrice>
+            </CarInfo>
+          </CarBasicInfoContainer>
+          <Specifications>
+            <Specification>
+              <SpeedIcon width={38} height={38} />
+              <SpecificationInfo>380km/h</SpecificationInfo>
+            </Specification>
+            <Specification>
+              <UpIcon width={38} height={38} />
+              <SpecificationInfo>3.2s</SpecificationInfo>
+            </Specification>
+            <Specification>
+              <StrenghtIcon width={38} height={38} />
+              <SpecificationInfo>800 HP</SpecificationInfo>
+            </Specification>
+            <Specification>
+              <FuelIcon width={38} height={38} />
+              <SpecificationInfo>Gasolina</SpecificationInfo>
+            </Specification>
+            <Specification>
+              <GearIcon width={38} height={38} />
+              <SpecificationInfo>Auto</SpecificationInfo>
+            </Specification>
+            <Specification>
+              <UserIcon width={38} height={38} />
+              <SpecificationInfo>2 pessoas</SpecificationInfo>
+            </Specification>
+          </Specifications>
+          <ScheduleContainer>
+            <DateContainer>
+              <Title>DE</Title>
+              <DateInfo>18 Julho 2020</DateInfo>
+            </DateContainer>
+            <ArrowRightIcon />
+            <DateContainer>
+              <Title>ATÉ</Title>
+              <DateInfo>20 Julho 2020</DateInfo>
+            </DateContainer>
+          </ScheduleContainer>
+          <Footer>
+            <PriceContainer>
+              <Title>TOTAL</Title>
+              <DayValue>R$ 580 x3 diárias</DayValue>
+            </PriceContainer>
+            <TotalValue>R$ 2,900</TotalValue>
+            <SubmitButton>
+              <SubmitButtonText>Alugar agora</SubmitButtonText>
+            </SubmitButton>
+          </Footer>
+        </Content>
+      </Container>
+    </>
+  );
+};
+export default CarDetails;
---
+++ src/screens/CarDetails/styles.ts
@@ -0,0 +1,143 @@
+import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/Feather';
+import {RectButton} from 'react-native-gesture-handler';
+
+import Speed from '../../assets/icons/speed.svg';
+import Strenght from '../../assets/icons/strenght.svg';
+import Up from '../../assets/icons/up.svg';
+import User from '../../assets/icons/user.svg';
+import Gas from '../../assets/icons/gas.svg';
+import Gear from '../../assets/icons/gear.svg';
+import Arrow from '../../assets/icons/arrow_right.svg';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+
+  padding: 20px;
+`;
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const BorderlessButton = styled.Pressable`
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 15px;
+
+  padding: 4px;
+`;
+export const GoBackIcon = styled(Icon)``;
+export const Pagination = styled.View`
+  flex-direction: row;
+`;
+
+export const Dot = styled.Text`
+  color: #47474d;
+  font-size: 24px;
+  margin-right: 3px;
+`;
+
+export const InactiveDot = styled.Text`
+  color: #aeaeb3;
+  font-size: 24px;
+
+  margin-right: 3px;
+  margin-left: 3px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+`;
+export const CarImageContainer = styled.View`
+  align-self: center;
+
+  height: 140px;
+
+  margin-top: 25px;
+  margin-bottom: 25px;
+`;
+export const CarImage = styled.Image`
+  flex: 1;
+`;
+export const CarBasicInfoContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const CarInfo = styled.View`
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+export const Title = styled.Text`
+  font-family: 'ArchivoNarrow-Medium';
+  font-size: 12px;
+  color: #aeaeb3;
+`;
+export const Info = styled.Text`
+  font-family: 'ArchivoNarrow-Medium';
+  font-size: 28px;
+  color: #47474d;
+`;
+
+export const DailyPrice = styled.Text`
+  font-family: 'ArchivoNarrow-Medium';
+  font-size: 25px;
+  color: #dc1637;
+`;
+
+export const Specifications = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  width: 100%;
+
+  margin-top: 10px;
+`;
+export const Specification = styled.View`
+  background-color: #f4f5f6;
+
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 15px;
+  padding: 20px;
+
+  width: 115px;
+  height: 115px;
+
+  border-bottom-width: 2px;
+  border-color: #ebebf0;
+`;
+export const SpeedIcon = styled(Speed)``;
+export const UpIcon = styled(Up)``;
+export const StrenghtIcon = styled(Strenght)``;
+export const FuelIcon = styled(Gas)`
+  color: #47474d;
+`;
+export const GearIcon = styled(Gear)``;
+export const UserIcon = styled(User)`
+  color: #47474d;
+`;
+export const SpecificationInfo = styled.Text`
+  font-family: 'Inter-Regular';
+  font-size: 13px;
+  color: #7a7a80;
+
+  margin-top: 14px;
+`;
+
+export const ScheduleContainer = styled.View``;
+export const DateContainer = styled.View``;
+export const DateInfo = styled.Text``;
+export const ArrowRightIcon = styled(Arrow)``;
+export const Footer = styled.View``;
+export const PriceContainer = styled.View``;
+export const DayValue = styled.Text``;
+export const TotalValue = styled.Text``;
+export const SubmitButton = styled(RectButton)``;
+export const SubmitButtonText = styled.Text``;
